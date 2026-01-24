519bc44be93e7a8aa93e7a1f1964d0a005f5702f
sunanta001 <sununtamintCH@gmail.com>
2026-01-24 14:36:41 +0700

    ย้อน

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 window.config = {
   WEB_APP_URL: "",
-  API_BASE_URL: "https://pmapi.ideatrade1.com/",
-  API_BASE:  "https://pmapi.ideatrade1.com/"
+  API_BASE_URL: "https://pmapi.ideatrade1.com/"
 };
